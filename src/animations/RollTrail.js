2d475ad39471c0ec06d98498e868e77954cabfbb
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-12-24 12:09:15 +0100

    first set

--- src/animations/RollTrail.js
+++ src/animations/RollTrail.js
@@ -1,41 +1,66 @@
 import React from 'react'
-import { animated, config, useTrail } from 'react-spring'
+import { animated, config, useTrail, interpolate, useSpring, useTransition } from 'react-spring'
 
-export default function SqueezeTrail({ children, state, delay = 0, x = 0, y = -50 }) {
+export default function RollTrail({ children, state, delay = 0, from, to, ...restProps }) {
   const items = React.Children.toArray(children)
 
-  const trail = useTrail(items.length, {
+  const { r } = useSpring({
+    from: { r: 90 },
+    to: { r: state ? 0 : 90 },
     config: config.default,
-    opacity: state ? 1 : 0,
-    x: state ? 0 : x,
-    y: state ? 0 : y,
+    delay,
+  })
+
+  const trail = useTrail(items.length, {
+    config: { mass: 1, tension: 2000, friction: 100 },
+    opacity: state ? to.o : from.o,
+    x: state ? to.x : from.x,
+    y: state ? to.y : from.y,
+    z: state ? to.z : from.z,
+
+    reset: false,
+    reverse: false,
     from: {
-      opacity: 0,
-      x,
-      y,
+      opacity: from.o,
+      x: from.x,
+      y: from.y,
+      z: from.z,
     },
     delay,
   })
 
   return (
     <>
-      {trail.map(({ x, y, ...rest }, index) => {
-        if (items[index] === ' ' && items[index].match(' ')) {
-          return ' '
-        }
-        return (
-          <animated.span
-            key={`${items[index]}_${index}`}
-            style={{
-              ...rest,
-              display: 'inline-flex',
-              transform: x.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
-            }}
-          >
-            {items[index]}
-          </animated.span>
-        )
-      })}
+      <animated.div
+        style={{
+          width: 'fit-content',
+          margin: '0 auto',
+          transformOrigin: `left`,
+          transform: r.interpolate((r) => `perspective(500px) rotateY(${r}deg)`),
+        }}
+      >
+        {trail.map(({ x, y, z, ...rest }, index) => {
+          if (items[index] === ' ' && items[index].match(' ')) {
+            return ' '
+          }
+          return (
+            <animated.span
+              key={`${items[index]}_${index}`}
+              {...restProps}
+              style={{
+                ...rest,
+                display: 'inline-flex',
+                transform: interpolate(
+                  [x, y, z],
+                  (x, y, z) => `perspective(500px) translate3d(0,${y}px,0) rotateX(${x}deg)`
+                ),
+              }}
+            >
+              {items[index]}
+            </animated.span>
+          )
+        })}
+      </animated.div>
     </>
   )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,17 @@
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled from 'styled-components/macro'
 import { mediaAbove } from '../utils/css-helpers'
+import { RollTrail } from '../animations'
 
-const ButtonLink = styled.a`
+const RotateDiv = styled.div`
+  transform: rotateY(80deg);
+  transform-origin: left;
+  width: fit-content;
+  margin: 0 auto;
+  transition: transform 0.5s ease-in;
+`
+
+const ButtonLinkWrapper = styled.a`
   position: relative;
   color: var(--blue-color);
   text-decoration: none;
@@ -14,6 +24,7 @@ const ButtonLink = styled.a`
   min-width: 200px;
   cursor: pointer;
   display: block;
+
   &:after {
     content: '';
     position: absolute;
@@ -32,6 +43,11 @@ const ButtonLink = styled.a`
     &:after {
       transform: scale(1.05);
     }
+
+    ${RotateDiv} {
+      transform: rotateY(0deg);
+    }
+
     //border: 1px solid var(--blue-color);
     //background: var(--blue-color);
   }
@@ -39,5 +55,39 @@ const ButtonLink = styled.a`
   
   `}
 `
+
+const ButtonLink = ({ children }) => {
+  const [hover, setHover] = useState(false)
+
+  return (
+    <ButtonLinkWrapper onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+      <div
+        css={`
+          display: ${hover ? 'none' : 'block'};
+        `}
+      >
+        {children.split('')}
+      </div>
+      <div
+        css={`
+          display: ${hover ? 'block' : 'none'};
+
+          /* position: absolute;
+          top: 0;
+          right: 0;
+          left: 0;
+          bottom: 0;
+          margin: auto;
+          width: 100%;
+          height: 100%; */
+        `}
+      >
+        <RollTrail from={{ y: 8, x: 90, o: 1, z: 0 }} to={{ y: 0, x: 0, o: 1, z: 0 }} state={hover}>
+          {children.split('')}
+        </RollTrail>
+      </div>
+    </ButtonLinkWrapper>
+  )
+}
 
 export default ButtonLink
